648947d71d7f82c4e6ca42a4f69588778778bff5
DavidArmenta <159225803+DavidArmenta@users.noreply.github.com>
2024-04-24 22:57:06 -0700

    first commit

---
+++ lib/presentation/notifications/notifications_event.dart
@@ -0,0 +1,8 @@
+part of 'notifications_bloc.dart';
+
+sealed class NotificationEvent extends Equatable {
+  const NotificationEvent();
+
+  @override
+  List<Object> get props => [];
+}
---
+++ lib/presentation/screen/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('push notification'),
+          actions: [
+            IconButton(onPressed: (){}, icon: const Icon(Icons.abc))
+          ],
+
+        )
+    );
+  }
+}
